40ff3096b08535fee8b2092451113e7d73f7f65d
JP_Billy <jannykarper@gmail.com>
2020-10-12 11:44:40 +0300

    12_10_20/11_44

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,7 +3,23 @@ import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.min.css';
 
-function Rewiews() {
+const options = {
+    margin: 30,
+    responsiveClass: true,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    navText: ["<", ">"],
+    loop: true,
+    responsive: {
+        0: {items: 1},
+        400: {items: 1},
+        600: {items: 2},
+    }
+
+};
+
+export default function Rewiews() {
     return <div className="site-section block-14 bg-light nav-direction-white">
         <div className="container">
             <div className="row mb-5">
@@ -13,10 +29,7 @@ function Rewiews() {
             </div>
             <OwlCarousel
                 className="owl-theme"
-                items="1"
-                autoplay
-                loop
-                nav
+                {...options}
 
             >
                     <div className="p-5">
@@ -71,10 +84,60 @@ function Rewiews() {
                             </div>
                         </div>
                     </div>
+                <div className="p-5">
+                    <div className="d-block block-testimony w-75 mx-auto text-center">
+                        <div className="person w-25 mx-auto mb-4">
+                            <img src="images/person_1.jpg" alt="Image" className="img-fluid rounded-circle"/>
+                        </div>
+                        <div>
+                            <h2 className="h5 mb-4">София Карташова</h2>
+                            <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+                                accusantium qui optio, possimus necessitatibus voluptate aliquam velit nostrum tempora
+                                ipsam!&rdquo;</blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-5">
+                    <div className="d-block block-testimony w-75 mx-auto text-center">
+                        <div className="person w-25 mx-auto mb-4">
+                            <img src="images/person_2.jpg" alt="Image" className="img-fluid rounded-circle"/>
+                        </div>
+                        <div>
+                            <h2 className="h5 mb-4">Артём Абрамов</h2>
+                            <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+                                accusantium qui optio, possimus necessitatibus voluptate aliquam velit nostrum tempora
+                                ipsam!&rdquo;</blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-5">
+                    <div className="d-block block-testimony w-75 mx-auto text-center">
+                        <div className="person w-25 mx-auto mb-4">
+                            <img src="images/person_3.jpg" alt="Image" className="img-fluid rounded-circle"/>
+                        </div>
+                        <div>
+                            <h2 className="h5 mb-4">Анна Рябова</h2>
+                            <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+                                accusantium qui optio, possimus necessitatibus voluptate aliquam velit nostrum tempora
+                                ipsam!&rdquo;</blockquote>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-5">
+                    <div className="d-block block-testimony w-75 mx-auto text-center">
+                        <div className="person w-25 mx-auto mb-4">
+                            <img src="images/person_4.jpg" alt="Image" className="img-fluid rounded-circle"/>
+                        </div>
+                        <div>
+                            <h2 className="h5 mb-4">Демид Матвеев</h2>
+                            <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+                                accusantium qui optio, possimus necessitatibus voluptate aliquam velit nostrum tempora
+                                ipsam!&rdquo;</blockquote>
+                        </div>
+                    </div>
+                </div>
             </OwlCarousel>
         </div>
     </div>
 
 }
-
-export default Rewiews;
--- src/components/SiteHalf.jsx
+++ src/components/SiteHalf.jsx
@@ -4,7 +4,7 @@ export default function SiteHalf() {
    return <div>
        <div className="container">
            <div className="row no-gutters align-items-stretch">
-               <div className="col-md-6 img-bg-1 pb-3">
+               <div className="col-md-6 img-bg-1">
                    <img src="images/hero_bg_2.jpg" width="100%"/>
                </div>
                <div className="col-md-6 ml-md-auto pl-lg-5">
@@ -21,17 +21,17 @@ export default function SiteHalf() {
 
        <div className="container">
            <div className="row no-gutters align-items-stretch">
-               <div className="col-md-6 img-bg-1 right">
-                   <img src="images/hero_bg_1.jpg" width="100%"/>
-               </div>
                <div className="col-md-5 mr-md-auto py-5">
-                   <span className="caption d-block mb-2 font-secondary font-weight-bold">Outstanding Services</span>
-                   <h2 className="site-section-heading text-uppercase font-secondary mb-5">Why Choose Us</h2>
+                   <span className="caption d-block mb-2 font-secondary font-weight-bold text-center">Outstanding Services</span>
+                   <h2 className="site-section-heading text-uppercase font-secondary mb-5 text-center">Why Choose Us</h2>
                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aliquid eius facilis
                        voluptatem eligendi magnam accusamus vel commodi asperiores sint rem reprehenderit nobis
                        nesciunt veniam qui fugit doloremque numquam quod.</p>
                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempora distinctio ipsam
                        nesciunt recusandae repellendus asperiores amet.</p>
+               </div>
+               <div className="col-md-6 img-bg-1">
+                   <img src="images/hero_bg_1.jpg" width="100%"/>
                </div>
            </div>
        </div>
